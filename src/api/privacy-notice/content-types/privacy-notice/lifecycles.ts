79b705a24d2c399d42e6b6c995759942b7d6a7ac
Andrei <andreigames9@gmail.com>
2023-10-29 22:47:02 +0100

    cache reset requests for pirvacy and legal notices

---
+++ src/api/privacy-notice/content-types/privacy-notice/lifecycles.ts
@@ -0,0 +1,24 @@
+import https from 'http';
+
+export default {
+  afterCreate() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+  afterCreateMany() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+
+  afterUpdate() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+  afterUpdateMany() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+
+  afterDelete() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+  afterDeleteMany() {
+    https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=privacyNotice&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+  },
+}
