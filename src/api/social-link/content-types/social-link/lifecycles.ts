574b5886cf1a082b732fa058ca0df1bf27babf27
Andrei <andreigames9@gmail.com>
2023-11-05 00:45:49 +0100

    Add lifecycles for social link content type

---
+++ src/api/social-link/content-types/social-link/lifecycles.ts
@@ -0,0 +1,42 @@
+import https from 'http';
+
+export default {
+  afterCreate() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+  afterCreateMany() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+
+  afterUpdate() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+  afterUpdateMany() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+
+  afterDelete() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+  afterDeleteMany() {
+    const req = https.get(`${process.env.NEXTJS_REVALIDATE_TAG_PATH}?tag=socialLink&secret=${process.env.NEXTJS_REVALIDATE_TAG_TOKEN}`);
+    req.on('error', (error) => {
+      console.error(error);
+    });
+  },
+}
