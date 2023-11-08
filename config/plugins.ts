a1593b9c969fafd88778f187eb8919067867f964
Andrei <andreigames9@gmail.com>
2023-11-08 21:42:46 +0100

    Add strapi-form-manager plugin to config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,9 @@
 export default {
   'strapi-plugin-populate-deep': {
     enabled: true,
+  },
+  'strapi-form-manager': {
+    enabled: true,
+    resolve: './src/plugins/strapi-form-manager'
   }
 }
