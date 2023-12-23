9e614e73b2956b4e692b012be4d687d00e7460f2
Andrei <andreigames9@gmail.com>
2023-12-23 16:54:51 +0100

    email config with nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,23 @@
-export default {
+export default ({ env }) => ({
   'strapi-plugin-populate-deep': {
     enabled: true,
+  },
+  'email': {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        secure: env('SMTP_SECURE'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        }
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_REPLYTO'),
+      }
+    }
   }
-}
+})
